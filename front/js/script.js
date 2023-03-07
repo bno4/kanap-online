d18b5c2d8def6d9000d900f804a0a07a92a0467e
bno4 <bnduchemin@gmail.com>
2023-03-07 14:49:33 +0100

    création de script.js et premier element du DOM

--- front/js/script.js
+++ front/js/script.js
@@ -3,25 +3,24 @@ const reponse = await fetch('http://localhost:3000/api/products');
 const products = await reponse.json();
 
 
-
-
 for (let i = 0; i < products.length; i++) {
 
     const article = products[i];
-
 
     const imageProduct = document.createElement("img");
     imageProduct.src = article.imageUrl;
     const nameProduct = document.createElement("h3");
     nameProduct.innerText = article.name;
-    const descriptionProduct = article.description("p")
+    const descriptionProduct = article.description("p");
+    descriptionProduct.innerText = article.description;
 
 }
 
 
-
+const sectionItems = document.querySelector(".items");
+const articleProduct = document.createElement("article");
 // récupération de l'élément du DOM qui accueillera les fiches produits
-const sectionItems = document.querySelector("#items")
-sectionItems.appendChild(imageProduct);
-sectionItems.appendChild(nameProduct);
-sectionItems.appendChild(descriptionProduct);
+sectionItems.appendChild(articleProduct);
+articleProduct.appendChild(imageProduct);
+articleProduct.appendChild(nameProduct);
+articleProduct.appendChild(descriptionProduct);
