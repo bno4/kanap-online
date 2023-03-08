7edbaa548314d14c8c2c3c8d813b1cc1c82d26b5
bno4 <bnduchemin@gmail.com>
2023-03-08 16:39:33 +0100

    canapés affiché sur la page d'accueil

--- front/js/script.js
+++ front/js/script.js
@@ -1,28 +1,38 @@
-// Récupération des pièces depuis le fichier JSON
-fetch("http://localhost:3000/api/products")
-    .then(response => response.json());
+const sectionProducts = document.querySelector("#items")
+console.log(sectionProducts)
 
+const URL = "http://localhost:3000/api/products"
 
+//********************* Récupération des pièces depuis l'API (source : https://developer.mozilla.org/en-US/docs/Web/API/Response/json) ************************/
 
+async function fetchData() {
+    fetch(URL)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            // création des éléments enfants de la sectionProducts ("#items")
+            for (const product of data) {
+                const linkProduct = document.createElement("a")
+                linkProduct.href = "./product.html?_id";
+                const articleProduct = document.createElement("article");
+                articleProduct.innerText;
+                const imageProduct = document.createElement("img");
+                imageProduct.src = product.imageUrl;
+                const nameProduct = document.createElement("h3");
+                nameProduct.innerText = product.name;
+                const descriptionProduct = document.createElement("p");
+                descriptionProduct.innerText = product.description;
+                sectionProducts.appendChild(linkProduct);
+                linkProduct.appendChild(articleProduct);
+                articleProduct.appendChild(imageProduct);
+                articleProduct.appendChild(nameProduct);
+                articleProduct.appendChild(descriptionProduct);
 
-for (let i = 0; i < products.length; i++) {
-
-    const article = products[i];
-
-    const imageProduct = document.createElement("img");
-    imageProduct.src = article.imageUrl;
-    const nameProduct = document.createElement("h3");
-    nameProduct.innerText = article.name;
-    const descriptionProduct = article.description("p");
-    descriptionProduct.innerText = article.description;
-
+            }
+        }
+        )
 }
 
+// appel de la fonction qui affichera les éléments
+fetchData()
 
-const sectionItems = document.querySelector(".items");
-const articleProduct = document.createElement("article");
-// récupération de l'élément du DOM qui accueillera les fiches produits
-sectionItems.appendChild(articleProduct);
-articleProduct.appendChild(imageProduct);
-articleProduct.appendChild(nameProduct);
-articleProduct.appendChild(descriptionProduct);
