2bbcc416fa0a7ac983ada0a12582681bdb84f244
bno4 <bnduchemin@gmail.com>
2023-03-13 18:07:21 +0100

    ajout des couleurs dans le menu déroulant

--- front/js/product.js
+++ front/js/product.js
@@ -9,17 +9,18 @@ const idProduct = searchParams.get("id")
 const urlProduct = `http://localhost:3000/api/products/${idProduct}`
 console.log(urlProduct);
 
+// Appel des éléments du produit
 function fetchProduct() {
     fetch(urlProduct)
         .then((response) => response.json())
         .then((object) => {
             console.log(object)
-            // affichage de la photo canapée
+            // affichage de la photo canapé
             const imageObject = document.createElement("img");
             imageObject.src = object.imageUrl;
             imageObject.alt = object.altTxt;
             document.querySelector(".item__img").appendChild(imageObject);
-            // affichage nom du canapée
+            // affichage nom du canapé
             const titleObject = document.getElementById("title");
             titleObject.innerText = object.name;
             // affichage du prix (ajout d'un espace entre le nbre et l'€)
@@ -30,8 +31,13 @@ function fetchProduct() {
             descriptionObject.innerText = object.description;
             // choix des couleurs
             const colorsObject = document.getElementById("colors");
-            // colorsObject.innerHTML = object.colors fonction map array
-            console.log(colorsObject);
+            // intégration des couleur dans le dropdown menu
+            object.colors.map(function (color) {
+                const colorOption = document.createElement("option");
+                colorsObject.appendChild(colorOption);
+                // colorOption.value = color;
+                colorOption.innerText = color;
+            });
         }
         )
 }
