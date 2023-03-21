18559c91627ce41adf888dd700d2bd1a72bb4ded
bno4 <bnduchemin@gmail.com>
2023-03-21 17:05:07 +0100

    mise en placemodification quantité

--- front/js/cart.js
+++ front/js/cart.js
@@ -13,38 +13,80 @@ if (objectInLocalStorage === null) {
   cartState.appendChild(emptyCart);
 
 } else {
-  //Si un produit est dans le panier boucle for pour pacourir le localstorage
+  //Si un produit est dans le panier boucle map pour pacourir le localstorage
   let productsInCart = [];
   // for (i = 0; i < objectInLocalStorage.length; i++) 
   objectInLocalStorage.map(product => {
-    console.log(objectInLocalStorage.length + `éléments dans le panier`);
+    console.log(objectInLocalStorage.length + ` éléments dans le panier`);
+    // création de la balise article et récupération des data du localstorage
+    const articleCart = document.createElement("article");
+    articleCart.className = 'cart__item';
+    articleCart.dataset.id = product.id;
+    articleCart.dataset.color = product.color;
 
-    document.querySelector('#cart__items').innerHTML += `<article class="cart__item" data-id="${product.id}" data-color="${product.color}">
-    <div class="cart__item__img">
-          <img src="${product.image}" alt="${product.altTxt}">
-        </div>
-        <div class="cart__item__content">
-          <div class="cart__item__content__description">
-            <h2>${product.name}</h2>
-            <p>${product.color}</p>
-            <p>${product.price} €</p>
-          </div>
-          <div class="cart__item__content__settings">
-            <div class="cart__item__content__settings__quantity">
-              <p>Qté : ${product.quantity} </p>
-              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-            </div>
-            <div class="cart__item__content__settings__delete">
-              <p class="deleteItem">Supprimer</p>
-            </div>
-          </div>
-        </div>
-      </article>`;
+    // création de la balise div avec img et récupération des data du localstorage
+    const divImgCart = document.createElement("div")
+    divImgCart.className = 'cart__item__img';
+    const imgCart = document.createElement("img");
+    imgCart.src = product.image;
+    imgCart.alt = product.alt_image;
 
+    // création du bloc nom, couleur, prix et récupération des data du localstorage
+    const divContent = document.createElement("div")
+    divContent.className = 'cart__item__content';
+    const divContentDescription = document.createElement("div")
+    divContentDescription.className = 'cart__item__content__description';
+    const titleItem = document.createElement('h2');
+    titleItem.innerText = product.name;
+    const colorItem = document.createElement('p');
+    colorItem.innerText = product.color;
+    const priceItem = document.createElement('p');
+    priceItem.innerText = `${product.price} €`;
 
+    // création du bloc d'ajustement de la quantité de canapés
+    const divContentSettings = document.createElement("div");
+    divContentSettings.className = 'cart__item__content__settings';
+    const divContentQty = document.createElement("div");
+    divContentQty.className = 'cart__item__content__settings__quantity';
+    const quantityItem = document.createElement('p');
+    quantityItem.innerText = "Qté :";
+    const setQuantity = document.createElement('input');
+    setQuantity.setAttribute("type", "number");
+    setQuantity.className = 'itemQuantity';
+    setQuantity.min = 0;
+    setQuantity.max = 100;
+    setQuantity.value = product.quantity;
+    const deleteDiv = document.createElement('div');
+    deleteDiv.className = 'cart__item__content__settings__delete';
+    const deleteButton = document.createElement('p');
+    deleteButton.innerText = "Supprimer";
+
+    // architecture HTML
+    cartState.appendChild(articleCart);
+    articleCart.appendChild(divImgCart);
+    divImgCart.appendChild(imgCart);
+    articleCart.appendChild(divContent);
+    divContent.appendChild(divContentDescription);
+    divContentDescription.append(titleItem, colorItem, priceItem);
+    divContent.appendChild(divContentSettings);
+    divContentSettings.appendChild(divContentQty);
+    divContentQty.append(quantityItem, setQuantity);
+    divContentSettings.appendChild(deleteDiv);
+    deleteDiv.appendChild(deleteButton);
   });
-  console.log(productsInCart);
-
-
 };
+
+// let inputs = document.querySelector('.itemQuantity');
+// inputs.addEventListener("change", (event) => {
+//   event.preventDefault();
+//   inputs.map(input => {
+//     input.addEventListener("change", (e) => {
+//       let selectedQuantity = e.target.value;
+//       if (selectedQuantity == 0 || selectedQuantity >= 100) {
+//         console.error("Merci de choisir une quantité entre 1 et 100");
+//         selectedQuantity = product.quantity;
+//       } 
+//   })
+
+// });
 
