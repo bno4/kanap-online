ab37e8d22bee7f98b1e016edaeccbcade524a136
bno4 <bnduchemin@gmail.com>
2023-04-03 10:31:46 +0200

    correction de confirmation.js, suppression de l'orderID après ouverture de la page

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,6 +1,7 @@
 // récupération et affichage de l'id saisie dans confirmation.html
 const orderId = document.getElementById('orderId');
 orderId.innerText = localStorage.getItem('orderId');
+
 // suppresiosn du localStorage de l'orderId
 localStorage.removeItem('orderId');
 
