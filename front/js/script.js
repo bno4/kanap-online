9ed7c13c5ec109059997ffcd20cb0b3cc49d3932
bno4 <bnduchemin@gmail.com>
2024-02-12 16:23:38 +0100

    essai sur render.com

--- front/js/script.js
+++ front/js/script.js
@@ -1,43 +1,44 @@
-const sectionProducts = document.querySelector("#items")
-console.log(sectionProducts)
+const sectionProducts = document.querySelector("#items");
+console.log(sectionProducts);
 
-const URL = "http://localhost:3000/api/products"
+const URL = "https://kanap-online.onrender.com";
+
+//
+//
+// "http://localhost:3000/api/products";
 
 //---- création de la fonction qui affiche les pièces depuis l'API
 
 function fetchData() {
-    fetch(URL)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-            // création des éléments enfants de la sectionProducts ("#items")
-            for (const product of data) {
-                const linkProduct = document.createElement("a");
-                linkProduct.href = `./product.html?id=${product._id}`;
-                const articleProduct = document.createElement("article");
-                articleProduct.innerText;
-                const imageProduct = document.createElement("img");
-                imageProduct.src = product.imageUrl;
-                imageProduct.alt = product.altTxt;
-                const nameProduct = document.createElement("h3");
-                nameProduct.innerText = product.name;
-                const descriptionProduct = document.createElement("p");
-                descriptionProduct.innerText = product.description;
-                sectionProducts.appendChild(linkProduct);
-                linkProduct.appendChild(articleProduct);
-                articleProduct.appendChild(imageProduct);
-                articleProduct.appendChild(nameProduct);
-                articleProduct.appendChild(descriptionProduct);
-                // articleProduct.appendChild(priceProduct);
-
-            }
-        }
-        )
-        .catch((error) => {
-            console.error("Error:", error);
-        });
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // création des éléments enfants de la sectionProducts ("#items")
+      for (const product of data) {
+        const linkProduct = document.createElement("a");
+        linkProduct.href = `./product.html?id=${product._id}`;
+        const articleProduct = document.createElement("article");
+        articleProduct.innerText;
+        const imageProduct = document.createElement("img");
+        imageProduct.src = product.imageUrl;
+        imageProduct.alt = product.altTxt;
+        const nameProduct = document.createElement("h3");
+        nameProduct.innerText = product.name;
+        const descriptionProduct = document.createElement("p");
+        descriptionProduct.innerText = product.description;
+        sectionProducts.appendChild(linkProduct);
+        linkProduct.appendChild(articleProduct);
+        articleProduct.appendChild(imageProduct);
+        articleProduct.appendChild(nameProduct);
+        articleProduct.appendChild(descriptionProduct);
+        // articleProduct.appendChild(priceProduct);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
 // appel de la fonction qui affichera les éléments
 fetchData();
-
